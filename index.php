79ab61810f2722f6f610b59cb6e6097acf3cb451
lucasclemerson <clemerson.lucas.oliveira@gmail.com>
2023-09-29 13:09:24 -0300

    Criação do PDF de exemplo

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+    require 'vendor/autoload.php';
+    use Dompdf\Dompdf;
+
+    $dompdf = new Dompdf();
+    $dompdf->loadHtml('hello world');
+
+    // (Optional) Setup the paper size and orientation
+    $dompdf->setPaper('A4', 'landscape');
+
+    // Render the HTML as PDF
+    $dompdf->render();
+
+    // Output the generated PDF to Browser
+    $dompdf->stream();
